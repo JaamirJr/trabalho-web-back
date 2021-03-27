b4ea1a2741878dfb1ae29bdb894abf5782c1a019
JaamirJr <junior.jamir@gmail.com>
2021-03-26 22:48:05 -0300

    ec2

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://deploy:jamir@cluster0.x2ssw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/trabalho_final', {
+mongoose.connect('mongodb://localhost/trabalho_final', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
